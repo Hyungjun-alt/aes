6966f1224872a0ef6a50c72835965081fcac0438
DESKTOP-H98ONMI\jun <gud2007213@gmail.com>
2025-02-15 13:40:55 +0900

    new feature added!

--- cpp/class.cpp
+++ cpp/class.cpp
@@ -10,10 +10,22 @@ namespace jun
         double x, y;
 
     public:
-        Point() : x(0), y(0) {}
+        Point(double x = 0, double y = 0) : x(x), y(y) {}
         void printPoint()
         {
             std::cout << x << ", " << y << std::endl;
+        }
+
+        void setPoint(double x, double y)
+        {
+            this->x = x;
+            this->y = y;
+        }
+
+        std::pair<double, double> getPoint()
+        {   
+            auto ret = std::make_pair(this->x, this->y);
+            return ret;
         }
     };
 }
@@ -21,4 +33,7 @@ namespace jun
 int main()
 {
     std::cout << "hello, world! " << std::endl;
+    jun::Point point(1,2);
+    point.printPoint();
+    point.getPoint();
 }
