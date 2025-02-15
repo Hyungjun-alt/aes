63e28854eba39ec70d61793b5b013507c7f8cd90
DESKTOP-H98ONMI\jun <gud2007213@gmail.com>
2025-02-15 12:06:58 +0900

    class added

--- cpp/class.cpp
+++ cpp/class.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <vector>
 
+namespace jun
+{
 
+    class Point
+    {
+    private:
+        double x, y;
 
-
+    public:
+        Point() : x(0), y(0) {}
+        void printPoint()
+        {
+            std::cout << x << ", " << y << std::endl;
+        }
+    };
+}
 
 int main()
 {
